99b498e04c69c87edf00d5db1731bada62665082
Bob D'Errico <bob@bobderrico.com>
2015-04-29 21:48:26 -0400

    Fixes gulpfile to that gulp serve will now open to app/index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 gulp.task('default', ['minifyCSS']);
 
 gulp.task('serve', ['minifyCSS'], function () {
-  browserSync({
-    proxy: 'localhost'
+  browserSync.init({
+    server: { 
+      baseDir: "./app", 
+    }
   });
   gulp.watch('./app/css/*.css', ['minifyCSS']);
-  gulp.watch('./*').on('change', reload);
+  gulp.watch('./**').on('change', reload);
 });
 
 gulp.task('minifyCSS', function() {
