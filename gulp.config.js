70f12e95ec4bfa814dafc7e722b11d612ec498a1
adobley <aidan@obley.net>
2015-06-03 09:15:06 -0400

    Added gulp.config and several tasks

---
+++ gulp.config.js
@@ -0,0 +1,71 @@
+module.exports = function() {
+  var clientPath     = './yadaApp/',
+      serverPath     = './server/',
+      serverApp      = './server.js',
+      buildPath      = './build/',
+      clientTemp     = clientPath + '.tmp/',
+      bowerPath      = clientPath + 'bower_components/';
+
+  var config = {
+    
+    defaultPort: 3000,
+    /**
+     * Generic Paths
+     */
+    // clientTemporary folder
+    clientTemp: clientTemp,
+    // Angular app root
+    clientPath: clientPath,
+    buildPath: buildPath,
+
+    /**
+     * Server Files
+     */
+    serverPath: serverPath,
+    serverApp: serverApp,
+
+    /**
+     * Client Files
+     */
+    // All js files
+    alljs: [
+      './*.js', // Root server folder JS
+      clientPath + '**/*.js', // Angular app root and sub
+      serverPath,
+      '!' + bowerPath + '/**'
+    ],
+    htmlTemplates: clientPath + '**/*.html',
+    index: clientPath + 'index.html',
+    fonts: bowerPath + 'font-awesome/fonts/**',
+    images: clientPath + 'images/**/*.*',
+    clientAssets: [
+      clientPath + '**/*.js', // Angular app root and sub
+      '!' + bowerPath + '/**'
+    ],
+
+    /**
+     * Bower
+     */
+    bower: {
+      json: require('./bower.json'),
+      directory: bowerPath,
+      ignorePath: ''
+    },
+
+    /**
+     * Browser Sync
+     */
+    browserReloadDelay: 1000
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+      bowerJson: config.bower.json,
+      directory: config.bower.directory,
+      ignorePath: config.bower.ignorePath
+    };
+    return options;
+  };
+
+  return config;
+};
