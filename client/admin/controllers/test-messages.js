500eae5d536d5404c29c3b7ab7025feff44f1358
Bob D'Errico <bob@bobderrico.com>
2015-07-19 12:29:35 -0400

    Adds logic to get test message data into the form

--- client/admin/controllers/test-messages.js
+++ client/admin/controllers/test-messages.js
@@ -4,8 +4,24 @@
 
   var AdminTestMessagesController = function($scope, YadaAPI) {
 
-  };
+    $scope.data = {};
 
+    $scope.getTestMessages = function() {
+      YadaAPI.testMessages.get().then(populate, function(err) {console.log(err);});
+    };
+
+    var populate = function(resp) {
+      var respData = resp.data[0];
+      $scope.data._id = respData._id;
+      $scope.data.satMessage = respData.satMessage;
+      $scope.data.satDetail = respData.satDetail;
+      $scope.data.actMessage = respData.actMessage;
+      $scope.data.actDetail = respData.actDetail;
+    };
+
+    $scope.getTestMessages();
+
+  };
 
   app.controller('AdminTestMessagesController', ['$scope', 'YadaAPI', AdminTestMessagesController]);
 
