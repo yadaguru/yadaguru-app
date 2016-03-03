a2ae957a325c21e8f4c05ffca86fa034fccb02c3
Bob D'Errico <bob@bobderrico.com>
2016-03-02 22:53:50 -0500

    Renames sms folder/controller to user. Reorganizes code on userController. Adds documentation

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,8 +2,50 @@ define(['app'], function (app) {
 
   'use strict';
 
+  /**
+   * Controller for user/sms setup
+   */
   app.register.controller('SmsController', ['$scope', '$cookies',
     function ($scope, $cookies) {
+
+      /**
+       * Updates user with phone number and advances sms setup.
+       */
+      $scope.submitMobile = function () {
+        //TODO POST to submit number to server
+        console.log('mobileNumber: ', $scope.mobileNumber);
+        $scope.smsSetupStep = 2;
+      };
+
+      /**
+       * Displays confirmation code message.
+       */
+      $scope.confirmCode = function () {
+        $scope.smsSetupStep = 3;
+      };
+
+      /**
+       * Updates the user with personal and sponsor code.
+       */
+      $scope.updateUser = function () {
+        //TODO POST to create new user for number
+        console.log('smsCode: ', $scope.smsCode);
+        console.log('personalCode: ', $scope.personalCode);
+        console.log('sponsorCode: ', $scope.sponsorCode);
+        $scope.isCompletionModalVisible = true;
+      };
+
+      /**
+       * Sets a cookie, indicating initial mobile setup has been completed.
+       */
+      $scope.endInitialMobileSetup = function () {
+        $cookies.put('initialSmsSetupComplete', true);
+      };
+
+      $scope.editLoginCode = function () {
+        $scope.smsSetupStep = 2;
+        $scope.showInitialSetup = true;
+      };
 
       $scope.$parent.showAdd = false;
       $scope.isCompletionModalVisible = false;
@@ -13,32 +55,7 @@ define(['app'], function (app) {
 
       $scope.smsSetupStep = 1;
 
-      $scope.submitMobile = function () {
-        //TODO POST to submit number to server
-        console.log('mobileNumber: ', $scope.mobileNumber);
-        $scope.smsSetupStep = 2;
-      };
 
-      $scope.confirmCode = function () {
-        $scope.smsSetupStep = 3;
-      };
-
-      $scope.registerUser = function () {
-        //TODO POST to create new user for number
-        console.log('smsCode: ', $scope.smsCode);
-        console.log('personalCode: ', $scope.personalCode);
-        console.log('sponsorCode: ', $scope.sponsorCode);
-        $scope.isCompletionModalVisible = true;
-      };
-
-      $scope.endInitialMobileSetup = function () {
-        $cookies.put('initialSmsSetupComplete', true);
-      };
-
-      $scope.editLoginCode = function () {
-        $scope.smsSetupStep = 2;
-        $scope.showInitialSetup = true;
-      };
 
     }]);
 
