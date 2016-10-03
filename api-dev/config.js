1b87fab1485c7afd3b452f32e05bc2cb2ef92825
Bob D'Errico <bob@bobderrico.com>
2016-10-02 22:21:37 -0400

    Removes api dev server and unneeded files. Updates api URL to use yadaguru-api dev URL

--- api-dev/config.js
+++
@@ -1,6 +0,0 @@
-var app = require('./app');
-
-module.exports = {
-  secret: 'changethisinproduction',
-  connectionString: 'postgres://yadaguru_api_dev:abcd1234@localhost:5432/yadaguru_api_dev'
-};
