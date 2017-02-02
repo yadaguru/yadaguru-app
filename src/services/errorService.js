ce45fe177d64cf6e3303443a46cb38e491b8fe3c
Bob D'Errico <bob@bobderrico.com>
2017-02-01 23:20:49 -0500

    Adds error modal to handleHttpError function

--- src/services/errorService.js
+++ src/services/errorService.js
@@ -1,10 +1,21 @@
 define(['app'], function(app) {
 
   app.factory('yg.services.error', [
-    function() {
+    'yg.services.modal',
+    function(modalService) {
       function handleHttpError(error) {
-        // TODO - Better error handling
-        console.log(error);
+        var modalMessage = modalService.makeModalMessage(
+          'OUR BAD!<br>Something went wrong. Could you just click here to refresh ' +
+          'the app?'
+        )
+        modalService.showModal(modalMessage, {
+          button: 'click here (sorry about that)'
+        })
+          .then(function() {
+            // TODO replace this with JSNLog
+            console.log(error);
+            location.reload();
+          });
       }
 
       return {
